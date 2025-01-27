44401cb1332d5d2d14936b56ca29044225395904
Sumit112234 <sumitbaghel22a@gmail.com>
2025-01-27 21:34:27 +0530

    3

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(cors());
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 // Routes
 app.use('/api/users', userRoutes);
